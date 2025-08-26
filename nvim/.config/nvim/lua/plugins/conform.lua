584fe9ad61f5f200a9f729e4a351fa137e90baff
Eric Hanley <eric@electiontechnologies.com>
2025-08-26 13:49:28 -0600

    adds leader lf

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,12 +1,21 @@
 return {
 	"stevearc/conform.nvim",
-	event = "BufWritePre",
-	opts = {
-		notify_on_error = true,
-		format_on_save = { timeout_ms = 500, lsp_fallback = false },
-		formatters_by_ft = {
-			python = { "ruff_format" },
-			lua = { "stylua" },
-		},
-	},
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
+
+		conform.setup({
+			notify_on_error = true,
+			format_on_save = { timeout_ms = 500, lsp_fallback = false },
+			formatters_by_ft = {
+				python = { "ruff_format" },
+				lua = { "stylua" },
+			},
+		})
+
+		-- Add format keymap
+		vim.keymap.set({ "n", "v" }, "<leader>lf", function()
+			conform.format({ async = true, lsp_fallback = true })
+		end, { desc = "Format buffer" })
+	end,
 }
